0012ad0e7a7570f2f4b37d5b0027c3c202beaa2c
John Blackshear <swjaybee@gmail.com>
2019-09-21 11:43:22 -0500

    create subscription page

--- src/components/CreateSub/CreateSub.js
+++ src/components/CreateSub/CreateSub.js
@@ -1,13 +1,47 @@
 import React from 'react';
 import Wrapper from '../SubCards/Wrapper';
+import './style.css'
 
 function CreateSub(props) {
     return (
         <Wrapper>
-            
-        </Wrapper>
+            <div className="container">
+            <form>
+                <div>
+                <div className="line1">
+                        <p>Subscription Name: </p><input type="text" size="50" name="store" onChange={props.onChange} value={props.store} />
+                    </div>
+
+                    <div className="line1">
+                        <p>Renewal Date:</p>
+                        <input type="date" size="50" name="date" onChange={props.onChange} value={props.date} />
+                    </div>
+                    <div className="line1">
+                        <p>Amount:</p>
+                        <div className="input-group-prepend">
+                            <span className="input-group-text">$</span>
+                            <input type="number" name="amount" onChange={props.onChange} value={props.amount}></input>
+                        <div className="line2">
+                        <form action="">
+                        <select name="">
+                            <option value="monthly">Monthly</option>
+                            <option value="annualy">Annualy</option>
+                        </select>
+                        </form>
+                        </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div>
+                    <p className="submitExpense"><input type="submit" onClick={props.onClick} /></p>
+                </div>
+            </form>
+        </div >
+    </Wrapper>
       
     )
 }
 
 export default CreateSub;
+
