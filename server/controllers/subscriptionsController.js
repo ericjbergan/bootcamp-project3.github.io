4f989d4e1827c506ce87d3de840508da3a5f6636
steliosk9 <steliosk9@gmail.com>
2019-09-24 19:22:19 -0500

    subs endpoints working

--- server/controllers/subscriptionsController.js
+++ server/controllers/subscriptionsController.js
@@ -3,27 +3,27 @@ const db = require("../models");
 module.exports = {
 
   findAll: function(req, res) {
-    db.subscription.find(req.query)
+    db.Subscription.find(req.query)
       .then(dbSubscription => res.json(dbSubscription))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.subscription.findById(req.params.id)
+    db.Subscription.findById(req.params.id)
       .then(dbSubscription => res.json(dbSubscription))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.subscription.create(req.body)
+    db.Subscription.create(req.body)
       .then(dbSubscription => res.json(dbSubscription))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.subscription.findOneAndUpdate({ id: req.params.id }, req.body)
+    db.Subscription.findOneAndUpdate({ id: req.params.id }, req.body)
       .then(dbSubscription => res.json(dbSubscription))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.subscription.findById(req.params.id)
+    db.Subscription.findById(req.params.id)
       .then(dbSubscription => dbSubscription.remove())
       .then(dbSubscription => res.json(dbSubscription))
       .catch(err => res.status(422).json(err));
