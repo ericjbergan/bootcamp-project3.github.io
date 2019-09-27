c0ba59e1e4aa96064c5cb0e395b4b61fa959c3eb
John Blackshear <swjaybee@gmail.com>
2019-09-26 21:38:06 -0500

    cards for each made

--- src/components/SubCards/Netflix.js
+++ src/components/SubCards/Netflix.js
@@ -11,43 +11,43 @@ const subscription = [
             "image": "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg",
             "url": "https://www.netflix.com/Login?nextpage=https%3A%2F%2Fwww.netflix.com%2FYourAccount"
             },
-//        {
-//             "name": "hulu",
-//             "renewal_date": "",
-//             "amount": "",
-//             "image": "https://upload.wikimedia.org/wikipedia/commons/e/e4/Hulu_Logo.svg",
-//             "url": "https://auth.hulu.com/web/login"
-//             },
-//        {
-//             "name": "amazon prime ",
-//             "renewal_date": "",
-//             "amount": "",
-//             "image": "https://upload.wikimedia.org/wikipedia/commons/e/e3/Amazon_Prime_Logo.svg",
-//             "url": "https://www.amazon.com/your-account"
-//             },
-//       {
-//             "name": "hbo now",
-//             "renewal_date": "",
-//             "amount": "",
-//             "image": "https://upload.wikimedia.org/wikipedia/commons/b/b2/HBONOW-logo.svg",
-//             "url":"https://play.hbonow.com/page/urn:hbo:page:home"
-//             },
-//         {
-//             "name": "sling tv",
-//             "renewal_date": "",
-//             "amount": "",
-//             "image": "https://upload.wikimedia.org/wikipedia/commons/5/53/Sling_TV_logo.svg",
-//             "url": "https://oauth-client.p.sling.com/?redirect_uri=https://www.sling.com/authentication_callback&client_id=ott_server&state=eqfk0"
+       {
+            "name": "hulu",
+            "renewal_date": "",
+            "amount": "",
+            "image": "https://upload.wikimedia.org/wikipedia/commons/e/e4/Hulu_Logo.svg",
+            "url": "https://auth.hulu.com/web/login"
+            },
+       {
+            "name": "amazon prime ",
+            "renewal_date": "",
+            "amount": "",
+            "image": "https://upload.wikimedia.org/wikipedia/commons/e/e3/Amazon_Prime_Logo.svg",
+            "url": "https://www.amazon.com/your-account"
+            },
+      {
+            "name": "hbo now",
+            "renewal_date": "",
+            "amount": "",
+            "image": "https://upload.wikimedia.org/wikipedia/commons/b/b2/HBONOW-logo.svg",
+            "url":"https://play.hbonow.com/page/urn:hbo:page:home"
+            },
+        {
+            "name": "sling tv",
+            "renewal_date": "",
+            "amount": "",
+            "image": "https://upload.wikimedia.org/wikipedia/commons/5/53/Sling_TV_logo.svg",
+            "url": "https://oauth-client.p.sling.com/?redirect_uri=https://www.sling.com/authentication_callback&client_id=ott_server&state=eqfk0"
 
-//             },
-//        {
-//             "name": "disney +",
-//             "renewal_date": "",
-//             "amount": "",
-//             "image": "https://upload.wikimedia.org/wikipedia/commons/3/3e/Disney%2B_logo.svg",
-//             "url": "https://preview.disneyplus.com"
+            },
+       {
+            "name": "disney +",
+            "renewal_date": "",
+            "amount": "",
+            "image": "https://upload.wikimedia.org/wikipedia/commons/3/3e/Disney%2B_logo.svg",
+            "url": "https://preview.disneyplus.com"
 
-//             },
+            },
         
     
    ];
@@ -59,27 +59,28 @@ class SubCard extends Component {
     return (
         // subscription.map(({i, image}) => <img key={i} src={image}  />)
         <div>
-        <div className="card">
          
-        {Object.keys(subscription).map(key => (
+        {subscription.map((sub, key) => (
+        <div className="card">
+
             <div  >
                 <div className="edit_remove">
                 <p className="edit">edit</p>
                 <p className="remove">remove</p>
                 </div>
                 
-            <a href ={subscription[0].url} key={key}>
+            <a href ={sub.url} key={key}>
 
-            {subscription[key].type}
-            <img src={subscription[0].image} key={key}  width="148px" height="100px"/>
+            {sub.type}
+            <img src={sub.image} key={key}  width="148px" height="100px"/>
 
       </a>
-            <p>{subscription[0].amount}/month</p>
-            <p>Subcription renews on: {subscription[0].renewal_date}</p>
+            <p>{sub.amount}/month</p>
+            <p>Subcription renews on: {sub.renewal_date}</p>
+            </div>
 
           </div>
         ))}
-      </div>
       </div>
       
     )
