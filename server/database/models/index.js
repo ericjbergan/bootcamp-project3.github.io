ef4c7473a336f045e6f7303a70b0e1a3f07fba58
Anh Thu Le <anhthu.le1926@gmail.com>
2019-09-30 13:28:58 -0500

    merging branches

--- server/database/models/index.js
+++
@@ -1,3 +0,0 @@
-// module.exports = {
-//   subscription: require("./Subscriptions")
-// };
--- server/routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-//1 require for each route type
-const subRoutes = require("./subscription");
-
-//one 'use()' for each route type
-router.use("/subscription", subRoutes);
-
-module.exports = router;
